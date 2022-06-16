20f229f32e19b730ec3d990d199a4615ee3ae5eb
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-16 14:36:25 +0300

    added event listeners to the like hearts

--- main.js
+++ main.js
@@ -3,10 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
-
+const likeHeart = document.querySelectorAll(".like-glyph")
+console.log(likeHeart)
+likeHeart.forEach( (elementLikeGlyph) => {
+  elementLikeGlyph.addEventListener('click',myheartFunc)
+})
+  
+function myheartFunc(event){
+  mimicServerCall()
+  .then(returnedserverMessage => {
+    let heart = event.target
+    if (heart.textContent === EMPTY_HEART)
+    {
+      heart.textContent = FULL_HEART
+      heart.textContent.classList.add
+    }else{
+      heart.textContent = EMPTY_HEART
+    }
+  })
+  .catch( (errorMessage) => {
+    let divContainerErrorMsg = document.getElementById(modal)
+    //let msgDisplay = divContainerErrorMsg.removeAttribute("hidden")
+    setTimeout(()=> {
+      let pTagMsg = document.querySelectorAll("p .modal-message")
+      pTagMsg.textContent = errorMessage
+    },3000)
+    
+  })
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
